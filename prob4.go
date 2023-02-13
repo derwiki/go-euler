ec0e628f3ada6d14bab1c3fe7fe9992dd23cc891
Adam Derewecki <a@der.wiki>
2023-02-12 17:44:12 -0800

    prob4.go

--- prob4.go
+++ prob4.go
@@ -1,38 +1,32 @@
 package main
+
 import (
-  "fmt"
-  "strconv"
+	"fmt"
+	"strconv"
 )
 
 func isFib(x int, y int) bool {
-  buffer := strconv.Itoa(x * y)
-  midpoint := len(buffer) / 2
-  //fmt.Println(midpoint)
-  for i := 0; i < midpoint; i++ {
-      //fmt.Println(i)
-      //fmt.Println(len(buffer) - 1 - i)
-      //fmt.Println(buffer[len(buffer) - 1 - i])
-      //fmt.Println("*****")
-      if buffer[len(buffer) - 1 - i] != buffer[i] {
-          return false
-      }
-      //fmt.Sprintf("%d\t%d", buffer[i], buffer[len(buffer) - i])
-  }
-  return true
+	buffer := strconv.Itoa(x * y)
+	midpoint := len(buffer) / 2
+	for i := 0; i < midpoint; i++ {
+		if buffer[len(buffer)-1-i] != buffer[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    x := 999
-    y := 999
-
-    for ;x>0; x--{
-       for ;y>0; y--{
-           if isFib(x, y) {
-               fmt.Println(fmt.Sprintf("%d, %d, %t", x, y, isFib(x, y)))
-           }
-       }
-    }
-    //fmt.Println(isFib(x, y))
-    //fmt.Println(isFib(91, 99))
-
+	largestPalindrome := 0
+	for x := 999; x > 99; x-- {
+		for y := 999; y > 99; y-- {
+			if isFib(x, y) {
+				fmt.Println(fmt.Sprintf("%d, %d, %d, %t", x, y, x*y, isFib(x, y)))
+				if x*y > largestPalindrome {
+					largestPalindrome = x * y
+				}
+			}
+		}
+	}
+	fmt.Println(fmt.Sprintf("Largest: %d", largestPalindrome))
 }
